2d7ff20fa64f1c5461f592524f47793de86b0848
Angelo Hunt <reachangelo@gmail.com>
2026-06-16 21:53:31 -0700

    style: force wedding stream to utilize the lower-third marquee ticker layout format

--- src/FuneralSlideshow.jsx
+++ src/FuneralSlideshow.jsx
@@ -1,6 +1,8 @@
+// src/FuneralSlideshow.jsx
 import React from 'react';
 import { MemorialSlideshowController } from './memorial/MemorialSlideshowController';
 
 export const FuneralSlideshow = (props) => {
+  // This forces whatever event ID is in the URL to run on the high-end marquee ticker layout
   return <MemorialSlideshowController {...props} />;
 };
